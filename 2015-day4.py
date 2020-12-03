a35191053a262df500ef17107ef79c2f53541f68
Milind Madhukar <m.milind15@gmail.com>
2020-12-03 11:48:35 +0530

    Code Added

---
+++ 2015-day4.py
@@ -0,0 +1,42 @@
+from main import getInputData
+import hashlib
+import threading
+from functools import partial
+
+def checkStart(start, num):
+    n = num
+    while True:
+        key = data[0] + str(n)
+        md5_hex = hashlib.md5(key.encode()).hexdigest()
+        if md5_hex.startswith(start):
+            print(md5_hex)
+            print(key)
+            exit()2020
+        # print(out, end='\r')
+        print(n, end='\r')
+        n += 1
+
+
+if __name__ == "__main__":
+    data = getInputData(year=2015,day=4)
+
+    t1 = threading.Thread(target=partial(checkStart, '000000',0))
+    t2 = threading.Thread(target=partial(checkStart, '000000',10000))
+    t3 = threading.Thread(target=partial(checkStart, '000000',20000))
+    t4 = threading.Thread(target=partial(checkStart, '000000',30000))
+    t5 = threading.Thread(target=partial(checkStart, '000000',40000))
+
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
+
+    
+        
---
+++ 2020-day3.py
@@ -0,0 +1,25 @@
+from typing import Counter
+from main import getInputData
+
+def getTreesEncountered(data, xStep, yStep):
+
+    x,y,count = 0,0,0
+    for _ in range(0,(len(data)-1)//yStep):
+        x += xStep
+        y += yStep
+
+        if data[y][x] == "#":
+            count += 1
+    
+    return count
+    
+if __name__ == "__main__":
+    data = getInputData()
+
+    for i in range(len(data)):
+        data[i] += data[i]*2000 #Hardcode Gang yeahhhh
+
+    # Stage 1
+    print("Number of trees encountered:",getTreesEncountered(data,3,1))
+    # Stage 2
+    print("Product of trees encountered:", getTreesEncountered(data,1,1)*getTreesEncountered(data,3,1)*getTreesEncountered(data,5,1)*getTreesEncountered(data,7,1)*getTreesEncountered(data,1,2))
