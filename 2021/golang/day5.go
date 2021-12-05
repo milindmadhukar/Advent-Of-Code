69c24400d2f2eae54aa65d357eb3723d87e13b7e
milindmadhukar <m.milind15@gmail.com>
2021-12-05 09:51:56 +0530

    added d5

---
+++ 2021/golang/day5.go
@@ -0,0 +1,17 @@
+package main
+
+func day5() {
+
+	data := getInputData(2021, 5)
+	day5part1(data)
+	day5part2(data)
+
+}
+
+func day5part1(data []string) {
+
+}
+
+func day5part2(data []string) {
+
+}
