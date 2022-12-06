cc2807205843d51b5aec8fee4791568e0bd200de
Milind <m.milind15@gmail.com>
2022-12-06 12:47:53 +0530

    modified day 6

--- 2022/python/day6.py
+++ 2022/python/day6.py
@@ -1,14 +1,12 @@
 from utils import getInputData
 
-contents = getInputData(year=2022, day=6, pure=True).strip()
+contents = getInputData(year=2022, day=6, pure=True)
 
 def check_unique_chars(till):
-    received_chars = []
     for idx in range(len(contents)-till):
-        received_chars.extend(contents[idx:idx+till])
+        received_chars = contents[idx:idx+till]
         if len(set(received_chars)) == len(received_chars):
             return idx + till
-        received_chars = []
 
 print("Answer for part 1", check_unique_chars(4))
 print("Answer for part 2", check_unique_chars(14))
