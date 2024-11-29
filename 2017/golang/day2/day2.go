593cc4b040c996375135aa2ff7bec99ca0333046
Milind <m.milind15@gmail.com>
2024-11-30 00:46:11 +0530

    2017/day2

---
+++ 2017/golang/day2/day2.go
@@ -0,0 +1,86 @@
+package day2
+
+import (
+	"math"
+	"time"
+
+	"github.com/milindmadhukar/Advent-Of-Code/2017/golang/utils"
+)
+
+type day1 struct {
+	data       []string
+	startTime  time.Time
+	parsedData [][]int
+}
+
+func (d day1) Part1() any {
+
+	checksum := 0
+
+	for _, line := range d.parsedData {
+		smallest := math.MaxInt
+		largest := math.MinInt
+		for _, num := range line {
+			if num > largest {
+				largest = num
+			}
+			if num < smallest {
+				smallest = num
+			}
+		}
+		checksum += (largest - smallest)
+	}
+
+	return checksum
+}
+
+func (d day1) Part2() any {
+	checksum := 0
+
+	for _, line := range d.parsedData {
+		found := false
+		for idx1, num1 := range line {
+			for idx2, num2 := range line {
+				if idx1 == idx2 {
+					continue
+				}
+				if num1%num2 == 0 {
+					found = true
+					checksum += num1 / num2
+					break
+				}
+			}
+			if found {
+				break
+			}
+		}
+	}
+
+	return checksum
+}
+
+func Solve() day1 {
+	data, err := utils.GetInputDataFromAOC(2017, 2)
+	if err != nil {
+		panic(err)
+	}
+
+	startTime := time.Now()
+
+	splitData := utils.GetSplitData(data, "\t")
+
+	var parsedData [][]int
+	for _, val := range splitData {
+		parsedData = append(parsedData, utils.StringSliceToIntegerSlice(val))
+	}
+
+	return day1{
+		data:       data,
+		startTime:  startTime,
+		parsedData: parsedData,
+	}
+}
+
+func (d day1) TimeTaken() time.Duration {
+	return time.Since(d.startTime)
+}
--- 2017/golang/solutions/solutions.go
+++ 2017/golang/solutions/solutions.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/milindmadhukar/Advent-Of-Code/2017/golang/day1"
+	"github.com/milindmadhukar/Advent-Of-Code/2017/golang/day2"
 	"github.com/milindmadhukar/Advent-Of-Code/2017/golang/models"
 )
 
@@ -12,6 +13,8 @@ func GetSolution(day int) models.Solution {
 	switch day {
 	case 1:
 		return day1.Solve()
+	case 2:
+		return day2.Solve()
 
 	default:
 		panic("Solution not implemented yet")
