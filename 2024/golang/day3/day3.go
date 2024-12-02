989fa8e7716fadfcc02b8b4668779ac7c3badbbd
Milind <m.milind15@gmail.com>
2024-12-02 11:44:53 +0530

    create day 3 for tomorrow

---
+++ 2024/golang/day3/day3.go
@@ -0,0 +1,44 @@
+package day3
+
+import (
+	"time"
+
+	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/utils"
+)
+
+type day3 struct {
+	data      []string
+	timeTaken time.Duration
+	startTime time.Time
+}
+
+func (d *day3) Part1() any {
+	return 0
+}
+
+func (d *day3) Part2() any {
+	return 0
+}
+
+func Solve() *day3 {
+	data, err := utils.GetInputDataFromAOC(2024, 3)
+	if err != nil {
+		panic(err)
+	}
+
+	// data = utils.GetInputDataFromFile("day3/example.txt")
+
+	startTime := time.Now()
+
+	endTime := time.Now()
+
+	return &day3{
+		data:      data,
+		startTime: startTime,
+		timeTaken: endTime.Sub(startTime),
+	}
+}
+
+func (d day3) TimeTaken() time.Duration {
+	return d.timeTaken
+}
