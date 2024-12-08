a695900ad20688343cdbafbc7794885d356ccb33
Milind <m.milind15@gmail.com>
2024-12-08 12:08:44 +0530

    day 8

--- 2024/golang/day8/day8.go
+++ 2024/golang/day8/day8.go
@@ -5,15 +5,71 @@ import (
 )
 
 type day8 struct {
-	data      []string
+	data     []string
+	antennas map[byte][]Point
+}
+
+type Point struct {
+	x int
+	y int
 }
 
 func (d *day8) Part1() any {
-	return 0
+	antiNodes := make(map[Point]bool)
+	for _, locations := range d.antennas {
+		pairs := utils.Combinations(locations, 2)
+		for _, pair := range pairs {
+			yDiff := pair[0].y - pair[1].y
+			xDiff := pair[0].x - pair[1].x
+
+			antiNode1 := Point{pair[0].x + xDiff, pair[0].y + yDiff}
+			antiNode2 := Point{pair[1].x - xDiff, pair[1].y - yDiff}
+
+			if antiNode1.x >= 0 && antiNode1.y >= 0 && antiNode1.x < len(d.data[0]) && antiNode1.y < len(d.data) {
+				antiNodes[antiNode1] = true
+			}
+			if antiNode2.x >= 0 && antiNode2.y >= 0 && antiNode2.x < len(d.data[0]) && antiNode2.y < len(d.data) {
+				antiNodes[antiNode2] = true
+			}
+		}
+	}
+
+	return len(antiNodes)
 }
 
 func (d *day8) Part2() any {
-	return 0
+	antiNodes := make(map[Point]bool)
+	for _, locations := range d.antennas {
+		pairs := utils.Combinations(locations, 2)
+		for _, pair := range pairs {
+			yDiff := pair[0].y - pair[1].y
+			xDiff := pair[0].x - pair[1].x
+
+			i := 0
+			for {
+				antiNode := Point{pair[0].x + (xDiff * i), pair[0].y + (yDiff * i)}
+				if antiNode.x >= 0 && antiNode.y >= 0 && antiNode.x < len(d.data[0]) && antiNode.y < len(d.data) {
+					antiNodes[antiNode] = true
+					i++
+				} else {
+					break
+				}
+			}
+
+			i = 0
+			for {
+				antiNode := Point{pair[1].x - (xDiff * i), pair[1].y - (yDiff * i)}
+				if antiNode.x >= 0 && antiNode.y >= 0 && antiNode.x < len(d.data[0]) && antiNode.y < len(d.data) {
+					antiNodes[antiNode] = true
+					i++
+				} else {
+					break
+				}
+			}
+		}
+	}
+
+	return len(antiNodes)
 }
 
 func Solve() *day8 {
@@ -24,8 +80,20 @@ func Solve() *day8 {
 
 	// data = utils.GetInputDataFromFile("day8/example.txt")
 
+	antennas := make(map[byte][]Point)
+
+	for y := 0; y < len(data); y++ {
+		for x := 0; x < len(data[y]); x++ {
+			if data[y][x] == '.' {
+				continue
+			}
+
+			antennas[data[y][x]] = append(antennas[data[y][x]], Point{x, y})
+		}
+	}
+
 	return &day8{
-		data:      data,
+		data:     data,
+		antennas: antennas,
 	}
 }
-
