01726deabe9a9e314397c06394cb07b108eac0a3
Milind <m.milind15@gmail.com>
2024-12-11 01:36:20 +0530

    benchmarking

--- 2024/golang/main_test.go
+++ 2024/golang/main_test.go
@@ -6,11 +6,20 @@ import (
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/solutions"
 )
 
-func BenchmarkDay09(b *testing.B) {
-	sol := solutions.GetSolution(9)
-
+func BenchmarkDay05(b *testing.B) {
+	sol := solutions.GetSolution(5)
 	for i := 0; i < b.N; i++ {
-    sol.Part1()
-    sol.Part2()
+		sol.Part1()
+		sol.Part2()
 	}
 }
+
+func BenchmarkDay09(b *testing.B) {
+	sol := solutions.GetSolution(9)
+	for i := 0; i < b.N; i++ {
+		sol.Part1()
+		sol.Part2()
+	}
+}
+
+// go test -bench=BenchmarkDay05 -cpuprofile profiles/cpu.pprof . 
