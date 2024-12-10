a58fb057dcdeae6136c26d1a16d39454a7426add
Milind <m.milind15@gmail.com>
2024-12-10 11:48:47 +0530

    Improving run capabilities

--- 2024/golang/README.md
+++ 2024/golang/README.md
@@ -2,12 +2,16 @@
 
 ### Instructions to run
 
-1. Create a "sessionkey.txt" and paste the `session` cookie
-1. Run the main file by `go run main.go`
+1. Create a "sessionkey.txt" at the same level as `main.go` and paste the `session` cookie (without the `session=`)
+1. To run all solutions, run `go run main.go --all`
+1. To run solution for a given day, Run `go run main.go --day <day>`
+1. If no flags are given, today's solution will be ran.
+
 
 #### You will find your session key by:
 1. Going to [Advent Of Code](https://adventofcode.com/)
 1. On your browser, Inspect Element `(Ctrl + Shift + I)`
 1. Click on Network Tab
 1. `Ctrl + R` to open network activity
-1. Scroll down and copy the value of session from cookies in request headers.
+1. Click on any request that goes to adventofcode.com
+1. Go to Request Headers, scroll down and copy the value of `session` from the **Cookies** part.
--- 2024/golang/main.go
+++ 2024/golang/main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/solutions"
 )
 
+func printHelp() {
+	fmt.Println("Usage:")
+	fmt.Println("  -day <number>: Run solution for a specific day")
+	fmt.Println("  -all: Run all solutions")
+	fmt.Println("  -help: Show this help message")
+	fmt.Println("If no flags are provided, today's solution will be run.")
+}
+
 func main() {
-	solutions.RunTodaysSolution()
+	day := flag.String("day", "", "Specify the day to run")
+	all := flag.Bool("all", false, "Run all solutions")
+	help := flag.Bool("help", false, "Show help message")
+	flag.Parse()
+
+	if *help {
+		printHelp()
+		return
+	}
+
+	if *all {
+		log.Println("Running all solutions")
+		solutions.RunAllSolutions()
+	} else if *day != "" {
+		dayInt, err := strconv.Atoi(*day)
+		if err != nil {
+			log.Fatalf("Invalid day: %s", *day)
+		}
+		solutions.RunSolutionForDay(dayInt)
+	} else {
+		solutions.RunTodaysSolution()
+	}
 }
