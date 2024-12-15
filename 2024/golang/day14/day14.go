4f88c26350e71a9fcb4bc15f6adb2191b5919151
Milind <m.milind15@gmail.com>
2024-12-15 08:14:34 +0530

    day 14 - was fun

--- 2024/golang/day14/day14.go
+++ 2024/golang/day14/day14.go
@@ -1,19 +1,102 @@
 package day14
 
 import (
+	"fmt"
+	"slices"
+	"strings"
+
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/utils"
 )
 
 type day14 struct {
-	data []string
+	data     []string
+	robots   []Robot
+	gridSize Point
+}
+
+type Point struct {
+	x, y int
+}
+
+type Robot struct {
+	position Point
+	velocity Point
 }
 
 func (d *day14) Part1() any {
-	return 0
+	robots := slices.Clone(d.robots)
+
+	secondsToSimulate := 100
+
+	for i := 0; i < secondsToSimulate; i++ {
+		for idx := range robots {
+			robots[idx].position.x = (robots[idx].position.x + robots[idx].velocity.x) % d.gridSize.x
+			robots[idx].position.y = (robots[idx].position.y + robots[idx].velocity.y) % d.gridSize.y
+
+			if robots[idx].position.x < 0 {
+				robots[idx].position.x = d.gridSize.x + robots[idx].position.x
+			}
+
+			if robots[idx].position.y < 0 {
+				robots[idx].position.y = d.gridSize.y + robots[idx].position.y
+			}
+		}
+	}
+
+	midX := d.gridSize.x / 2
+	midY := d.gridSize.y / 2
+	var firstQuadrant, secondQuadrant, thirdQuadrant, fourthQuadrant int
+
+	for _, robot := range robots {
+		if robot.position.x > midX && robot.position.y < midY {
+			firstQuadrant++
+		} else if robot.position.x < midX && robot.position.y < midY {
+			secondQuadrant++
+		} else if robot.position.x < midX && robot.position.y > midY {
+			thirdQuadrant++
+		} else if robot.position.x > midX && robot.position.y > midY {
+			fourthQuadrant++
+		}
+	}
+
+	return firstQuadrant * secondQuadrant * thirdQuadrant * fourthQuadrant
 }
 
 func (d *day14) Part2() any {
-	return 0
+
+	robots := slices.Clone(d.robots)
+
+	var allPositions []map[Point]bool
+
+	count := 0
+	for {
+		positions := make(map[Point]bool)
+		for idx := range robots {
+			robots[idx].position.x = (robots[idx].position.x + robots[idx].velocity.x) % d.gridSize.x
+			robots[idx].position.y = (robots[idx].position.y + robots[idx].velocity.y) % d.gridSize.y
+
+			if robots[idx].position.x < 0 {
+				robots[idx].position.x = d.gridSize.x + robots[idx].position.x
+			}
+
+			if robots[idx].position.y < 0 {
+				robots[idx].position.y = d.gridSize.y + robots[idx].position.y
+			}
+			positions[robots[idx].position] = true
+		}
+
+		allPositions = append(allPositions, positions)
+
+		count++
+
+		if len(positions) == len(robots) {
+			break
+		}
+	}
+
+	visualize(d.gridSize, allPositions[count-6:])
+
+	return count
 }
 
 func Solve() *day14 {
@@ -22,9 +105,22 @@ func Solve() *day14 {
 		panic(err)
 	}
 
-	data = utils.GetInputDataFromFile("day14/example.txt")
+	var robots []Robot
+
+	for _, line := range data {
+		split := strings.Split(line, " ")
+		var robot Robot
+		fmt.Sscanf(split[0], "p=%d,%d", &robot.position.x, &robot.position.y)
+		fmt.Sscanf(split[1], "v=%d,%d", &robot.velocity.x, &robot.velocity.y)
+		robots = append(robots, robot)
+	}
+
+	// gridSize := Point{11, 7}
+	gridSize := Point{101, 103}
 
 	return &day14{
-		data: data,
+		data:     data,
+		robots:   robots,
+		gridSize: gridSize,
 	}
 }
---
+++ 2024/golang/day14/visualizer.go
@@ -0,0 +1,85 @@
+package day14
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+func randomColor() color.RGBA {
+	source := rand.NewSource(time.Now().UnixNano())
+	rand := rand.New(source)
+	return color.RGBA{
+		R: uint8(rand.Intn(256)),
+		G: uint8(rand.Intn(256)),
+		B: uint8(rand.Intn(256)),
+		A: 255,
+	}
+}
+
+func processRow(y int, xMax int, upscaledImageBy int, clusterColors map[Point]color.RGBA, upscaled *image.Paletted, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for x := 0; x < xMax; x++ {
+		for i := 0; i < upscaledImageBy; i++ {
+			for j := 0; j < upscaledImageBy; j++ {
+				upscaled.Set(x*upscaledImageBy+i, y*upscaledImageBy+j, clusterColors[Point{x, y}])
+			}
+		}
+	}
+}
+
+func visualize(gridSize Point, allPositions []map[Point]bool) {
+	upscaleImageBy := 10
+	var frames []*image.Paletted
+	var delays []int
+
+	var palette = []color.Color{
+		color.RGBA{0x00, 0x00, 0x00, 0xff},
+		color.RGBA{0, 240, 50, 255},
+	}
+
+	count := 0
+	delayPerFrame := 25
+
+	for _, positionMap := range allPositions {
+		robotPositionColours := make(map[Point]color.RGBA)
+		for pos := range positionMap {
+			robotPositionColours[pos] = color.RGBA{0, 240, 50, 255}
+		}
+
+		upscaled := image.NewPaletted(
+			image.Rect(0, 0, gridSize.x*upscaleImageBy, gridSize.y*upscaleImageBy),
+			palette,
+		)
+		var wg sync.WaitGroup
+		for y := 0; y < gridSize.y; y++ {
+			wg.Add(1)
+			go processRow(y, gridSize.x, upscaleImageBy, robotPositionColours, upscaled, &wg)
+		}
+		wg.Wait()
+		frames = append(frames, upscaled)
+		delays = append(delays, delayPerFrame)
+
+		count++
+	}
+
+	outputFile, err := os.Create("day14/robots.gif")
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	err = gif.EncodeAll(outputFile, &gif.GIF{
+		Image: frames,
+		Delay: delays,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}
