15e7a55fdf99a5946e7ad269351de3e29d13691a
Milind <m.milind15@gmail.com>
2024-12-16 01:09:42 +0530

    day 15 uh such dirty code

--- 2024/golang/day15/day15.go
+++ 2024/golang/day15/day15.go
@@ -1,7 +1,7 @@
 package day15
 
 import (
-	"os"
+	"fmt"
 	"strings"
 
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/utils"
@@ -19,39 +19,56 @@ type Point struct {
 	x, y int
 }
 
-func (d *day15) PrintGrid() {
+func (d *day15) PrintGridPart1() {
 	for y := 0; y < d.gridSize.y; y++ {
 		for x := 0; x < d.gridSize.x; x++ {
 			if wallFound := d.walls[Point{x, y}]; wallFound {
-				print("#")
+				fmt.Print("#")
 			} else if boxFound := d.boxes[Point{x, y}]; boxFound {
-				print("O")
+				fmt.Print("O")
 			} else if d.robot.x == x && d.robot.y == y {
-				print("@")
+				fmt.Print("@")
 			} else {
-				print(".")
+				fmt.Print(".")
 			}
 		}
-		println()
+		fmt.Println()
 	}
-	println()
+	fmt.Println()
 }
 
-func (d *day15) MoveBoxes(dx, dy int, boxes map[Point]bool) {
+func (d *day15) PrintGridPart2(boxes map[Point]string) {
+	for y := 0; y < d.gridSize.y; y++ {
+		for x := 0; x < 2*d.gridSize.x; x++ {
+			if wallFound := d.walls[Point{x, y}]; wallFound {
+				fmt.Print("#")
+			} else if boxFound := boxes[Point{x, y}]; boxFound != "" {
+				fmt.Print(boxFound)
+			} else if d.robot.x == x && d.robot.y == y {
+				fmt.Print("@")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+
+}
+
+func (d *day15) MoveBoxesPart1(dx, dy int) {
 	if wallFound := d.walls[Point{d.robot.x + dx, d.robot.y + dy}]; wallFound {
 		return
 	}
 
 	var boxesToMove []Point
-	hitAWall := false
 
 	tempPos := d.robot
 	for {
 		tempPos = Point{tempPos.x + dx, tempPos.y + dy}
-		if boxFound := boxes[tempPos]; boxFound {
+		if boxFound := d.boxes[tempPos]; boxFound {
 			if wallFound := d.walls[Point{tempPos.x + dx, tempPos.y + dy}]; wallFound {
-				hitAWall = true
-				break
+				return
 			}
 			boxesToMove = append(boxesToMove, tempPos)
 		} else {
@@ -59,42 +76,47 @@ func (d *day15) MoveBoxes(dx, dy int, boxes map[Point]bool) {
 		}
 	}
 
-	if hitAWall {
-		return
+	for _, box := range boxesToMove {
+		delete(d.boxes, box)
 	}
 
 	for _, box := range boxesToMove {
-		delete(boxes, box)
-	}
-
-	for _, box := range boxesToMove {
-		boxes[Point{box.x + dx, box.y + dy}] = true
+		d.boxes[Point{box.x + dx, box.y + dy}] = true
 	}
 
 	d.robot = Point{d.robot.x + dx, d.robot.y + dy}
 }
 
 func (d *day15) Part1() any {
-	boxes := make(map[Point]bool)
+	boxesOriginal := make(map[Point]bool)
 	for k, v := range d.boxes {
-		boxes[k] = v
+		boxesOriginal[k] = v
 	}
+
+	var robotOrignal Point = d.robot
+	fmt.Println("Original Robot", robotOrignal)
 
 	for _, move := range d.moves {
 		switch move {
 		case "<":
-			d.MoveBoxes(-1, 0, boxes)
+			d.MoveBoxesPart1(-1, 0)
 		case ">":
-			d.MoveBoxes(1, 0, boxes)
+			d.MoveBoxesPart1(1, 0)
 		case "^":
-			d.MoveBoxes(0, -1, boxes)
+			d.MoveBoxesPart1(0, -1)
 		case "v":
-			d.MoveBoxes(0, 1, boxes)
+			d.MoveBoxesPart1(0, 1)
 		}
+
+		// fmt.Println("Current Move", move)
+		// d.PrintGrid(d.gridSize)
 	}
 
+	d.robot = robotOrignal
+	d.boxes = boxesOriginal
+
 	sum := 0
-	for box, isPresent := range boxes {
+	for box, isPresent := range d.boxes {
 		if isPresent {
 			sum += (100 * box.y) + box.x
 		}
@@ -103,8 +125,136 @@ func (d *day15) Part1() any {
 	return sum
 }
 
+func (d *day15) MoveBoxesPart2(dx, dy int, boxes map[Point]string) {
+	if wallFound := d.walls[Point{d.robot.x + dx, d.robot.y + dy}]; wallFound {
+		return
+	}
+
+	boxesToMove := make(map[Point]string)
+	tempPos := d.robot
+
+	if dy == 0 {
+		for {
+			tempPos = Point{tempPos.x + dx, tempPos.y + dy}
+			if boxVal := boxes[tempPos]; boxVal != "" {
+				if wallFound := d.walls[Point{tempPos.x + dx, tempPos.y + dy}]; wallFound {
+					return
+				}
+				boxesToMove[tempPos] = boxVal
+			} else {
+				break
+			}
+		}
+	} else {
+		for {
+			// Check phase
+			for box := range boxesToMove {
+				if wallFound := d.walls[Point{box.x + dx, box.y + dy}]; wallFound {
+					return
+				}
+			}
+
+			// Add phase
+			if len(boxesToMove) == 0 {
+				if boxFound := boxes[Point{tempPos.x + dx, tempPos.y + dy}]; boxFound != "" {
+					if boxFound == "[" {
+						boxesToMove[Point{tempPos.x, tempPos.y + dy}] = "["
+						boxesToMove[Point{tempPos.x + +1, tempPos.y + dy}] = "]"
+					} else if boxFound == "]" {
+						boxesToMove[Point{tempPos.x - 1, tempPos.y + dy}] = "["
+						boxesToMove[Point{tempPos.x, tempPos.y + dy}] = "]"
+					}
+				} else {
+					break
+				}
+			} else {
+				nextBoxes := make(map[Point]string)
+				for k, v := range boxesToMove {
+					nextBoxes[k] = v
+				}
+				oldLen := len(nextBoxes)
+				for box := range boxesToMove {
+					if boxFound := boxes[Point{box.x + dx, box.y + dy}]; boxFound != "" {
+						if boxFound == "[" {
+							nextBoxes[Point{box.x + dx, box.y + dy}] = "["
+							nextBoxes[Point{box.x + 1, box.y + dy}] = "]"
+						} else if boxFound == "]" {
+							nextBoxes[Point{box.x - 1, box.y + dy}] = "["
+							nextBoxes[Point{box.x, box.y + dy}] = "]"
+						}
+					}
+				}
+				if len(nextBoxes) == oldLen {
+					break
+				} else {
+					for k, v := range nextBoxes {
+						boxesToMove[k] = v
+					}
+				}
+			}
+		}
+	}
+
+	for box := range boxesToMove {
+		delete(boxes, box)
+	}
+
+	for box, boxVal := range boxesToMove {
+		boxes[Point{box.x + dx, box.y + dy}] = boxVal
+	}
+
+	d.robot = Point{d.robot.x + dx, d.robot.y + dy}
+}
+
 func (d *day15) Part2() any {
-	return 0
+	walls := make(map[Point]bool)
+	boxes := make(map[Point]string)
+
+	for k, v := range d.walls {
+		walls[Point{2 * k.x, k.y}] = v
+		walls[Point{2*k.x + 1, k.y}] = v
+	}
+
+	for k := range d.boxes {
+		boxes[Point{2 * k.x, k.y}] = "["
+		boxes[Point{2*k.x + 1, k.y}] = "]"
+	}
+
+	d.robot = Point{2 * d.robot.x, d.robot.y}
+
+	for p := range d.walls {
+		delete(d.walls, p)
+	}
+	for p := range d.boxes {
+		delete(d.boxes, p)
+	}
+
+	d.walls = walls
+
+	for _, move := range d.moves {
+		switch move {
+		case "<":
+			d.MoveBoxesPart2(-1, 0, boxes)
+		case ">":
+			d.MoveBoxesPart2(1, 0, boxes)
+		case "^":
+			d.MoveBoxesPart2(0, -1, boxes)
+		case "v":
+			d.MoveBoxesPart2(0, 1, boxes)
+		}
+
+		// fmt.Println("Current Move", move)
+		// d.PrintGridPart2(boxes)
+	}
+
+	sum := 0
+	for box, isPresent := range boxes {
+		if isPresent == "[" {
+			sum += (100 * box.y) + box.x
+		}
+	}
+
+	return sum
 }
 
 func Solve() *day15 {
@@ -113,10 +263,12 @@ func Solve() *day15 {
 		panic(err)
 	}
 
-	fileData, _ := os.ReadFile("day15/example.txt")
-	data = string(fileData)
-	data = strings.Trim(data, " ")
-	data = strings.Trim(data, "\n")
+	/*
+		fileData, _ := os.ReadFile("day15/example.txt")
+		data = string(fileData)
+		data = strings.Trim(data, " ")
+		data = strings.Trim(data, "\n")
+	*/
 
 	splitData := strings.Split(data, "\n\n")
 	grid := utils.GetSplitData(strings.Split(splitData[0], "\n"), "")
