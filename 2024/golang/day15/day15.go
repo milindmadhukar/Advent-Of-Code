2612e01553ec07442a353ab5e3ff1a9a93c3bc76
Milind <m.milind15@gmail.com>
2024-12-15 11:12:23 +0530

    part 1

--- 2024/golang/day15/day15.go
+++ 2024/golang/day15/day15.go
@@ -1,15 +1,103 @@
 package day15
 
 import (
+	"strings"
+
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/utils"
 )
 
 type day15 struct {
-	data []string
+	walls    map[Point]bool
+	boxes    map[Point]bool
+	robot    Point
+	moves    []string
+	gridSize Point
+}
+
+type Point struct {
+	x, y int
+}
+
+func (d *day15) PrintGrid() {
+	for y := 0; y < d.gridSize.y; y++ {
+		for x := 0; x < d.gridSize.x; x++ {
+			if wallFound := d.walls[Point{x, y}]; wallFound {
+				print("#")
+			} else if boxFound := d.boxes[Point{x, y}]; boxFound {
+				print("O")
+			} else if d.robot.x == x && d.robot.y == y {
+				print("@")
+			} else {
+				print(".")
+			}
+		}
+		println()
+	}
+	println()
+}
+
+func (d *day15) MoveBoxes(dx, dy int) {
+	if wallFound := d.walls[Point{d.robot.x + dx, d.robot.y + dy}]; wallFound {
+		return
+	}
+
+	var boxesToMove []Point
+	hitAWall := false
+
+	tempPos := d.robot
+
+	for {
+		tempPos = Point{tempPos.x + dx, tempPos.y + dy}
+		if boxFound := d.boxes[tempPos]; boxFound {
+			if wallFound := d.walls[Point{tempPos.x + dx, tempPos.y + dy}]; wallFound {
+				hitAWall = true
+				break
+			}
+			boxesToMove = append(boxesToMove, tempPos)
+		} else {
+			break
+		}
+	}
+
+	if hitAWall {
+		return
+	}
+
+	for _, box := range boxesToMove {
+		delete(d.boxes, box)
+	}
+
+	for _, box := range boxesToMove {
+		d.boxes[Point{box.x + dx, box.y + dy}] = true
+	}
+
+	d.robot = Point{d.robot.x + dx, d.robot.y + dy}
 }
 
 func (d *day15) Part1() any {
-	return 0
+	for _, move := range d.moves {
+
+		switch move {
+		case "<":
+			d.MoveBoxes(-1, 0)
+		case ">":
+			d.MoveBoxes(1, 0)
+		case "^":
+			d.MoveBoxes(0, -1)
+		case "v":
+			d.MoveBoxes(0, 1)
+		}
+	}
+
+	sum := 0
+
+	for box, present := range d.boxes {
+		if present {
+			sum += (100 * box.y) + box.x
+		}
+	}
+
+	return sum
 }
 
 func (d *day15) Part2() any {
@@ -17,14 +105,47 @@ func (d *day15) Part2() any {
 }
 
 func Solve() *day15 {
-	data, err := utils.GetInputDataFromAOC(2024, 15)
+	data, err := utils.GetRawInputDataFromAOC(2024, 15)
 	if err != nil {
 		panic(err)
 	}
 
-	// data = utils.GetInputDataFromFile("day15/example.txt")
+	/*
+		fileData, _ := os.ReadFile("day15/example.txt")
+		data = string(fileData)
+		data = strings.Trim(data, " ")
+		data = strings.Trim(data, "\n")
+	*/
+
+	splitData := strings.Split(data, "\n\n")
+	grid := utils.GetSplitData(strings.Split(splitData[0], "\n"), "")
+	moves := strings.Split(splitData[1], "")
+	walls := make(map[Point]bool)
+	boxes := make(map[Point]bool)
+	var robot Point
+
+	for y, row := range grid {
+		for x, cell := range row {
+			if cell == "#" {
+				walls[Point{x, y}] = true
+			} else if cell == "O" {
+				boxes[Point{x, y}] = true
+			} else if cell == "@" {
+				robot = Point{x, y}
+			}
+		}
+	}
+
+	maxY := len(grid)
+	maxX := len(grid[0])
+
+	gridSize := Point{maxX, maxY}
 
 	return &day15{
-		data: data,
+		walls:    walls,
+		boxes:    boxes,
+		robot:    robot,
+		moves:    moves,
+		gridSize: gridSize,
 	}
 }
