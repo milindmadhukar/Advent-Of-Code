e695d38bfb3d98c11b6e44abc8fbb23854a28ff8
Milind <m.milind15@gmail.com>
2024-12-20 11:46:24 +0530

    part 1

--- 2024/golang/day20/day20.go
+++ 2024/golang/day20/day20.go
@@ -1,15 +1,94 @@
 package day20
 
 import (
+	"strings"
+
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/utils"
 )
 
 type day20 struct {
-	data []string
+	maze      [][]string
+	start     Point
+	end       Point
+	pathSteps int
+	parents   map[Point]Point
+}
+
+type Point struct {
+	x, y int
+}
+
+type QueueItem struct {
+	point Point
+	steps int
+}
+
+var directions = []Point{{0, 1}, {0, -1}, {1, 0}, {-1, 0}}
+
+func (d *day20) BFS() (int, map[Point]Point) {
+	queue := []QueueItem{{point: d.start, steps: 0}}
+	visited := make(map[Point]bool)
+	next := make(map[Point]Point)
+
+	visited[d.start] = true
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		if current.point == d.end {
+			return current.steps, next
+		}
+
+		for _, dir := range directions {
+			nextPoint := Point{current.point.x + dir.x, current.point.y + dir.y}
+			if nextPoint.x < 0 || nextPoint.x > len(d.maze[0])-1 || nextPoint.y < 0 || nextPoint.y > len(d.maze)-1 {
+				continue
+			}
+
+			if d.maze[nextPoint.y][nextPoint.x] == "#" || visited[nextPoint] {
+				continue
+			}
+
+			visited[nextPoint] = true
+			queue = append(queue, QueueItem{point: nextPoint, steps: current.steps + 1})
+			next[nextPoint] = current.point
+		}
+	}
+
+	return -1, nil
 }
 
 func (d *day20) Part1() any {
-	return 0
+
+	timeSaved := make(map[int]int)
+
+	for y, line := range d.maze {
+		for x, char := range line {
+			if x == 0 || y == 0 || x == len(d.maze[0])-1 || y == len(d.maze)-1 {
+				continue
+			}
+
+			if char == "#" && char != "S" && char != "E" {
+				d.maze[y][x] = "."
+				steps, _ := d.BFS()
+				d.maze[y][x] = "#"
+				if steps < d.pathSteps {
+					timeSaved[d.pathSteps-steps]++
+				}
+			}
+		}
+	}
+
+	cheatsThatSaveTimeLessThan100ps := 0
+
+	for k, v := range timeSaved {
+    if k >= 100 {
+      cheatsThatSaveTimeLessThan100ps += v
+    }
+	}
+
+	return cheatsThatSaveTimeLessThan100ps
 }
 
 func (d *day20) Part2() any {
@@ -24,7 +103,29 @@ func Solve() *day20 {
 
 	// data = utils.GetInputDataFromFile("day20/example.txt")
 
-	return &day20{
-		data: data,
+	var maze [][]string
+
+	var start, end Point
+
+	for y, line := range data {
+		currentLine := strings.Split(line, "")
+		for x, char := range currentLine {
+			if char == "S" {
+				start = Point{x, y}
+			} else if char == "E" {
+				end = Point{x, y}
+			}
+		}
+		maze = append(maze, currentLine)
 	}
+
+	d := day20{
+		maze:  maze,
+		start: start,
+		end:   end,
+	}
+
+	d.pathSteps, d.parents = d.BFS()
+
+	return &d
 }
--- 2024/golang/main_test.go
+++ 2024/golang/main_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/day06"
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/day09"
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/day11"
+	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/day19"
 	"github.com/milindmadhukar/Advent-Of-Code/2024/golang/models"
 )
 
@@ -41,6 +42,15 @@ func BenchmarkDay11(b *testing.B) {
 	var sol models.Solution
 	for i := 0; i < b.N; i++ {
 		sol = day11.Solve()
+		sol.Part1()
+		sol.Part2()
+	}
+}
+
+func BenchmarkDay19(b *testing.B) {
+	var sol models.Solution
+	for i := 0; i < b.N; i++ {
+		sol = day19.Solve()
 		sol.Part1()
 		sol.Part2()
 	}
